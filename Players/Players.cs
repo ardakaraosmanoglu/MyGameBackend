9d30d73bc885e6375acd5a399a18337463f1a118
Arda Karaosmanoglu <ardakaraosmanoglu7@gmail.com>
2021-01-30 18:20:58 +0300

    Add project files.

---
+++ Players/Players.cs
@@ -0,0 +1,21 @@
+﻿namespace MyGameBackend
+{
+    class Players 
+    {
+        public int Id { get; set; }
+        public string Nickname { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Birth { get; set; }
+    }
+
+    class PlayerManager
+    {
+        public void Registration
+        {
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyGameBackend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
